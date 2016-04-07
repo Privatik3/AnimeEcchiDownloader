56cf69036adf4d10b11318aadf38509002f97d60
Privatik3 <lol1192@yandex.ru>
2016-04-07 16:33:48 +0300

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-       // Document doc = Jsoup.connect("http://anime.reactor.cc/tag/ecchi/best/1").get();
-      
+        Document doc = Jsoup.connect("http://anime.reactor.cc/tag/ecchi/best/1").get();
+        Elements newsHeadlines = doc.select(".blogs.super li");
+
+        for (Element element: newsHeadlines) {
+            System.out.println(element.select("strong").text());
+        }
     }
 }

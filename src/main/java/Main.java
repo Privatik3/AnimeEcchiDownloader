a8a0a0fc13148d1ffa52b5b97c7f90ea2eb81b73
XIIIAngel <XIIIAngelXIIIAngel@yandex.ru>
2016-04-07 16:30:19 +0300

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Document doc = Jsoup.connect("http://anime.reactor.cc/tag/ecchi/best/1").get();
-        Elements newsHeadlines = doc.select(".image");
-
-        for (Element element: newsHeadlines) {
-            System.out.println(element.getElementsByTag("img"));
-        }
+       // Document doc = Jsoup.connect("http://anime.reactor.cc/tag/ecchi/best/1").get();
+      
     }
 }

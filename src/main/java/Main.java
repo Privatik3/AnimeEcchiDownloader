1e33a86dcb160c02885b07db0c4313a65c853506
Privatik3 <lol1192@yandex.ru>
2016-04-07 14:58:17 +0300

    Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Document doc = Jsoup.connect("http://anime.reactor.cc/tag/ecchi/best/1").get();
+        Elements newsHeadlines = doc.select(".image");
+
+        for (Element element: newsHeadlines) {
+            System.out.println(element.getElementsByTag("img"));
+        }
+    }
+}

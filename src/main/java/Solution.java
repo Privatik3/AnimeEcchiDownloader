21e8de6366b01de47b264e16aeece34698b5e116
Privatik3 <lol1192@yandex.ru>
2016-04-12 10:25:00 +0300

    Commit

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,5 +2,13 @@ public class Solution {
 
     public void parseTagImg(String body) {
 
+        String[] allDivImage = body.split("class=\"image");
+        for (String bodyDiv : allDivImage) {
+            int startUrlIndex = bodyDiv.indexOf("img src=");
+            int endUrlIndex = bodyDiv.indexOf("jpeg\" width");
+
+            if (endUrlIndex != -1)
+                System.out.println(bodyDiv.substring(startUrlIndex + 9, endUrlIndex + 4));
+        }
     }
 }
